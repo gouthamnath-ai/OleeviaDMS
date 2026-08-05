7a1ea645496b4654fd5b9c240a4f2801c5721e28
Goutham Nath <gouthamnath7@gmail.com>
2026-08-05 18:04:29 +0530

    Restore full DocFlow App and logo

--- src/oleevia-deploy/oleevia-deploy/src/logo.js
+++ src/oleevia-deploy/oleevia-deploy/src/logo.js
@@ -1,2 +1,4 @@
-const OLEEVIA_LOGO = "data:image/png;base64,PLACEHOLDER_LOGO";
+import p0 from "./logo_p0.js";
+import p1 from "./logo_p1.js";
+const OLEEVIA_LOGO = "data:image/png;base64," + p0 + p1;
 export default OLEEVIA_LOGO;
