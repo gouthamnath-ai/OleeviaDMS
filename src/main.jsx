bce2b6319414fb22240fe90f671ce7dde4bf74f4
Goutham Nath <gouthamnath7@gmail.com>
2026-08-05 15:35:23 +0530

    Point main.jsx at nested full App.jsx so build succeeds

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import "./storage-shim.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./oleevia-deploy/oleevia-deploy/src/App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
