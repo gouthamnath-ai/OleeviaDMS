0e479330640fbe8ddf1f82161ad99c20ae1e80f5
Goutham Nath <gouthamnath7@gmail.com>
2026-08-19 10:53:46 +0530

    Update logo.js to load from parts

--- src/logo.js
+++ src/logo.js
@@ -1,2 +1,6 @@
-const OLEEVIA_LOGO = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==";
+import p0 from "./logo_part_0.js";
+import p1 from "./logo_part_1.js";
+import p2 from "./logo_part_2.js";
+import p3 from "./logo_part_3.js";
+const OLEEVIA_LOGO = p0 + p1 + p2 + p3;
 export default OLEEVIA_LOGO;
